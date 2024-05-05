f754b1ee7b729b3171250dbc0634650c596f9f38
kishan2910 <kishanajudiya13@gmail.com>
2024-05-05 18:30:52 +0200

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from src.mlopsProject import logger  --> no need because it's already mentioned in constructor file (__init__.py)
 from mlopsProject import logger
 from mlopsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlopsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -8,6 +9,16 @@ try:
         obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
